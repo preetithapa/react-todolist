8e585cf0183dc8bf43c1e253b336fc532ac92cb5
preetithapa <preeti20@navgurukul.org>
2022-06-13 12:54:52 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import User from './components/User';
+import ToDoLists from './components/ToDoLists';
+
+function App() {
+  return (
+    <div className="App">
+      <User />
+      <ToDoLists />
+    </div>
+  );
+}
+
+export default App;
